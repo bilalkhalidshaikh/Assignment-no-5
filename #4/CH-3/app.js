029f8b85d62e0cae58baf5ab48bc706984acdd73
bilalkhalidshaikh <64368766+bilalkhalidshaikh@users.noreply.github.com>
2020-06-14 17:35:09 +0500

    Add files via upload

---
+++ #4/CH-3/app.js
@@ -0,0 +1,20 @@
+var age;
+var visitor;
+var birthYear;
+var visName;
+var proTitle;
+var quant;
+
+age = "Iam 15 years old";
+visitor = ("You have visit this site 16 times");
+birthYear = "My birth year is 2003 <br> Data type of my declared variable is number" + "<br>" + "<br>";
+visName = "<strong> Jhon Doe </strong>";
+quant = "ordered <strong> 5 T-shirt </strong>(s)";
+proTitle = " on XYZ Clothing Store";
+
+alert(age);
+alert(visitor);
+
+document.write(birthYear);
+document.write(visName + quant + proTitle);
+
